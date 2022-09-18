127436cc0ab94d7ca0aec1b8200b1304208fd9c2
Malek Hamza <66556288+mlhmz@users.noreply.github.com>
2022-09-18 08:13:33 +0000

    Adjusted title margin

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -13,7 +13,7 @@ const nav = reactive({
 <template>
     <nav class="fixed">
         <div class="flex flex-wrap justify-between items-center mx-auto w-screen py-4 md:p-3 bg-slate-900 bg-opacity-50 border-b border-slate-800 backdrop-blur-lg">
-            <RouterLink to="/" class="font-bold text-xl flex self-center ml-1">🧑‍💻 <span class="text-white">Malek Hamza</span></RouterLink>
+            <RouterLink to="/" class="font-bold text-xl flex self-center ml-5 md:ml-0">🧑‍💻 <span class="text-white">Malek Hamza</span></RouterLink>
             <div class="block md:hidden">
                 <button class="flex items-center px-3 py-2 mx-7 border rounded text-teal-200 border-primary hover:text-secondary hover:bg-primary hover:shadow-teal-800"
                 v-on:click="toggleNav()">
