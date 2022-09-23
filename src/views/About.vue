f273ad3aadfe401904e3e18ad2399c5c3f3d917d
Malek <malek.hamza@tuta.io>
2022-09-23 16:24:30 +0200

    Added placeholders to about site

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,24 @@
+<script setup>
+    import { LoremIpsum } from "lorem-ipsum";
+
+    const lorem = new LoremIpsum({
+        sentencesPerParagraph: {
+            max: 8,
+            min: 4
+        },
+        wordsPerSentence: {
+            max: 16,
+            min: 4
+        }
+    })
+</script>
+
 <template>
-    <div class="about">
-        <h1 class="text-center">Coming soon</h1>
+    <div class="about w-1/2 m-auto">
+        <h1 class="text-center text-2xl font-bold">About me</h1>
+        <div class="bg-white w-40 h-40 rounded-md m-auto my-5">
+        
+        </div>
+        <p>{{lorem.generateSentences(3)}}</p>
     </div>
 </template>
