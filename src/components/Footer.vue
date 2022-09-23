a648b8a051130da13320f3dc06b306b375a9fa23
Malek <malek.hamza@tuta.io>
2022-09-23 16:23:32 +0200

    Added content to header

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,12 +5,13 @@
     <div class="text-center container text-slate-400 border-t border-slate-800 m-auto my-5 p-5">
         <p>Malek Hamza — 2022</p>
         <p>Made with <font-awesome-icon icon="fa-regular fa-heart" /> + <font-awesome-icon icon="fa-brands fa-vuejs" /> </p>
-        <p>Since there are no commercial activities or opinions on this website, I see no need for an imprint here</p>
-        <p>If you disagree, feel free to contact me.
-            <span class="text-primary" 
+        <p class="mt-1">Since there are no commercial activities or opinions on this website, I see no need for an imprint here</p>
+        <p>If you disagree, feel free to contact me.</p>
+        <a class="text-primary" 
             href="mailto:malek.hamza@tuta.io">
                 malek.hamza@tuta.io
-            </span>
-        </p>
+        </a>
+        <p class="mt-1">Also, the webpage sources can be found on Github licensed with MIT</p>
+        <a class="text-primary" href="https://github.com/mlhmz/homepage">Link to Github Repo</a>
     </div>
 </template>
