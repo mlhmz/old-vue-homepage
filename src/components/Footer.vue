201d08fa068c9c1e1d88e2f8d55f79d5bda177c5
Malek <malek.hamza@tuta.io>
2022-09-23 16:11:54 +0200

    changed formulation of disclaimer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,7 +5,7 @@
     <div class="text-center container text-slate-400 border-t border-slate-800 m-auto my-5 p-5">
         <p>Malek Hamza — 2022</p>
         <p>Made with <font-awesome-icon icon="fa-regular fa-heart" /> + <font-awesome-icon icon="fa-brands fa-vuejs" /> </p>
-        <p>Since there are no commercial activities and opinions on this website, I see no need for an imprint here</p>
+        <p>Since there are no commercial activities or opinions on this website, I see no need for an imprint here</p>
         <p>If you disagree, feel free to contact me.
             <span class="text-primary" 
             href="mailto:malek.hamza@tuta.io">
