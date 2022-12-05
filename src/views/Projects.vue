5d7dbe574789c0d4c9d8ceff35b006ff5d9b2b41
Malek <malek.hamza@gmx.de>
2022-12-05 23:40:43 +0100

    Added PocketBase as dependency and wrote first code with it

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,3 +1,15 @@
+<script setup>
+import PocketBase from 'pocketbase';
+
+const pb = new PocketBase('http://127.0.0.1:8090');
+
+const projects = await pb.collection('homepage_projects').getFullList();
+
+for (project in projects) {
+    console.log(project.id)
+}
+</script>
+
 <template>
     <div class="projects">
         <h1 class="text-center">Coming soon</h1>
